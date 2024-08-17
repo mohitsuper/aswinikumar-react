683614158df4ea620091d1b40a0cffafb25d5094
mohitsuper <animemusically6@gamil.com>
2024-08-17 16:38:56 +0530

    yes

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/',
-  plugins: [
-    react(),
-  ],
+  plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['react-router-dom'],
-    }, 
+      // Remove 'react-router-dom' from here
+    },
   },
-
-})
+});
