14bbdc3d23c47f4d738f1f8f90c8280bcc67dd8d
mohitsuper <animemusically6@gamil.com>
2024-08-17 16:34:12 +0530

    yes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,5 @@
 import './App.css'
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-} from "react-router-dom";
+import { BrowserRouter as Router, Route,Routes } from 'react-router-dom';
 import Home from './pages/home/Home'
 import Error from './pages/nopage/Error'
 function App() {
